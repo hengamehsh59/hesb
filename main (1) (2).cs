3201600de5e9718fa132eef756cd36af11e39940
hengamehsh59 <hengamehsh59@gmail.com>
2024-09-26 20:47:20 +0330

    Add files via upload

---
+++ main (1) (2).cs
@@ -0,0 +1,50 @@
+using System;
+
+public struct Calculator
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("You Can't Divide Number By Zero");
+            return 0;
+        }
+else{
+        return a / b;
+    }}
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        Console.WriteLine("Enter First Number :");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Number :");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Addition: {calculator.Add(num1, num2)}");
+        Console.WriteLine($"Subtraction: {calculator.Subtract(num1, num2)}");
+        Console.WriteLine($"Multiplication: {calculator.Multiply(num1, num2)}");
+        Console.WriteLine($"Division: {calculator.Divide(num1, num2)}");
+    }
+}
+
